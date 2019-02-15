d7d865a8fff037b92ac5100b7cab03b7064b0e98
Pruthvi77 <47656657+Pruthvi77@users.noreply.github.com>
2019-02-15 13:19:01 +0530

    Create 18gromacs.sh

---
+++ 18gromacs.sh
@@ -0,0 +1,14 @@
+git clone https://github.com/gromacs/gromacs && cd gromacs
+cd ~/Desktop/rocm/workloads/miniapps/gromacs
+
+echo AH64_uh1 | sudo rm -rf build
+mkdir build && cd build
+cmake .. -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON > ~/Desktop/logs/18gromacs_build.log 2>&1
+
+make -j 8  >> ~/Desktop/logs/18gromacs_test.log 2>&1
+make -j 8 check > ~/Desktop/logs/18gromacs_test.log 2>&1
+
+
+
+
+
