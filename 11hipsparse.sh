66164277fb8f0a8f726258105b26dc4ad4002a55
Pruthvi77 <47656657+Pruthvi77@users.noreply.github.com>
2019-02-15 13:16:18 +0530

    Create 11hipsparse.sh

---
+++ 11hipsparse.sh
@@ -0,0 +1,14 @@
+git clone -b master https://github.com/rocmsoftwareplatform/hipsparse && cd hipsparse
+cd ~/Desktop/rocm/workloads/mathlibs/hipsparse
+
+echo AH64_uh1 | sudo yum -y --force-yes install cmake cmake-qt-gui cmake-curses-gui python-yaml libblas-dev liblapack-dev libboost-all-dev
+
+echo AH64_uh1 | sudo rm -rf build
+echo AH64_uh1 | sudo ./install.sh -cd >> ~/Desktop/logs/11hipsparse_build.log 2>&1
+
+cd build/release/clients/tests
+./hipsparse-test >> ~/Desktop/logs/11hipsparse_unittest.log 2>&1
+
+
+
+
