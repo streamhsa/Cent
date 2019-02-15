cd21701bbe6151a866ef22dc025c922568754081
Pruthvi77 <47656657+Pruthvi77@users.noreply.github.com>
2019-02-15 13:15:52 +0530

    Create 9rocprim.sh

---
+++ 9rocprim.sh
@@ -0,0 +1,12 @@
+git clone -b master https://github.com/rocmsoftwareplatform/rocprim && cd rocprim
+cd ~/Desktop/rocm/workloads/mathlibs/rocprim
+
+echo AH64_uh1 | sudo rm -rf build
+mkdir build; cd build
+
+CXX=/opt/rocm/hcc/bin/hcc cmake -DBUILD_BENCHMARK=OFF -DDISABLE_WERROR=ON ../. >> ~/Desktop/logs/9rocprim_build.log 2>&1
+make -j4 >> ~/Desktop/logs/9rocprim_build.log 2>&1
+
+#To run unit tests
+ctest --output-on-failure >> ~/Desktop/logs/9rocprim_ctest.log 2>&1
+
