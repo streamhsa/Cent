0f8c4ce709d339e740f83ed9911a42b1780c36c8
Pruthvi77 <47656657+Pruthvi77@users.noreply.github.com>
2019-02-15 13:16:31 +0530

    Create 12rocalution.sh

---
+++ 12rocalution.sh
@@ -0,0 +1,13 @@
+git clone -b master https://github.com/rocmsoftwareplatform/rocalution && cd rocalution
+cd ~/Desktop/rocm/workloads/mathlibs/rocalution
+
+echo AH64_uh1 | sudo yum -y install cmake cmake-qt-gui cmake-curses-gui python-yaml libblas-dev liblapack-dev libboost-all-dev
+
+echo AH64_uh1 | sudo rm -rf build
+echo AH64_uh1 | sudo ./install.sh -cd >> ~/Desktop/logs/12rocalution_build.log 2>&1
+
+cd build/release/clients/tests
+./rocalution-test >> ~/Desktop/logs/12rocalution_unittest.log 2>&1
+
+
+
