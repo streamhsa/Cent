3ca3a88ef94e810c57b5d485cfd384704969df4c
Pruthvi77 <47656657+Pruthvi77@users.noreply.github.com>
2019-02-15 13:14:56 +0530

    Create 6hipblas.sh

---
+++ 6hipblas.sh
@@ -0,0 +1,14 @@
+git clone -b master https://github.com/rocmsoftwareplatform/hipblas && cd hipblas
+cd ~/Desktop/rocm/workloads/mathlibs/hipblas
+
+echo AH64_uh1 | sudo yum -y --force-yes install cmake cmake-qt-gui cmake-curses-gui python-yaml libblas-dev liblapack-dev libboost-all-dev
+
+echo AH64_uh1 | sudo rm -rf build
+echo AH64_uh1 | sudo ./install.sh -cd >> ~/Desktop/logs/6hipblas_build.log 2>&1
+
+cd build/release/clients/staging
+./hipblas-test >> ~/Desktop/logs/6hipblas_unittest.log 2>&1
+./example-sscal >> ~/Desktop/logs/6hipblas_example-sscal.log 2>&1
+./example-sgemm >> ~/Desktop/logs/6hipblas_example-sgemm.log 2>&1
+./example-sgemm-strided-batched >> ~/Desktop/logs/6hipblas_example-sgemm-strided-batched.log 2>&1
+
