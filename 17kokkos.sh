58ff4ed07cc0665f10a63b7f4281bb26c7a57b91
Pruthvi77 <47656657+Pruthvi77@users.noreply.github.com>
2019-02-15 13:40:21 +0530

    Update 17kokkos.sh

--- 17kokkos.sh
+++ 17kokkos.sh
@@ -1,4 +1,8 @@
-git clone -b master https://github.com/kokkos/kokkos && cd kokkos
+#sudo rm -rf cd ~/Desktop/rocm/workloads/miniapps/kokkos
+#cd ~/Desktop/rocm/workloads/miniapps/
+#git clone -b master https://github.com/kokkos/kokkos && cd kokkos
+
+
 cd ~/Desktop/rocm/workloads/miniapps/kokkos
 
 echo AH64_uh1 | sudo rm -rf build
@@ -7,8 +11,3 @@ echo AH64_uh1 | sudo rm -rf build
 mkdir build && cd build
 ~/Desktop/rocm/workloads/miniapps/kokkos/generate_makefile.bash --compiler=/opt/rocm/hcc/bin/clang++ --arch=Fiji --with-rocm --no-examples --make -j4 >> ~/Desktop/logs/17kokkos_build.log 2>&1
 make test >> ~/Desktop/logs/17kokkos_test.log 2>&1
-
-
-
-
-
