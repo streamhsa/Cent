0acef322b13fef06923d1eac4f0f9355ca4f1ed2
Pruthvi77 <47656657+Pruthvi77@users.noreply.github.com>
2019-02-15 13:40:52 +0530

    Update 18gromacs.sh

--- 18gromacs.sh
+++ 18gromacs.sh
@@ -1,4 +1,8 @@
-git clone https://github.com/gromacs/gromacs && cd gromacs
+#sudo rm -rf cd ~/Desktop/rocm/workloads/miniapps/gromacs
+#cd ~/Desktop/rocm/workloads/miniapps/
+#git clone https://github.com/gromacs/gromacs && cd gromacs
+
+
 cd ~/Desktop/rocm/workloads/miniapps/gromacs
 
 echo AH64_uh1 | sudo rm -rf build
@@ -7,8 +11,3 @@ cmake .. -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON > ~/Desktop/logs/1
 
 make -j 8  >> ~/Desktop/logs/18gromacs_test.log 2>&1
 make -j 8 check > ~/Desktop/logs/18gromacs_test.log 2>&1
-
-
-
-
-
